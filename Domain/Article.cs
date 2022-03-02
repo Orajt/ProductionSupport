7f2a7744f077d88e2327ce8c36e9ba51c64cf10c
Orajt <marcin.jasiukiewicz1993@gmail.com>
2022-03-02 09:42:25 +0100

    Chaanges in domain, remove unnecessary fields

--- Domain/Article.cs
+++ Domain/Article.cs
@@ -2,6 +2,30 @@ namespace Domain
 {
     public class Article
     {
+        public Article()
+        {
+
+        }
+        public Article(string name, string nameWithoutFamilly, ArticleType articleType, Familly familly, Stuff stuff, bool createdInCompany)
+        {
+            this.FullName=name;
+            this.NameWithoutFamilly=nameWithoutFamilly;
+            this.ArticleTypeId=articleType.Id;
+            this.ArticleType=articleType;
+            if(familly!=null)
+            {
+                this.Familly=familly;
+                this.FamillyId=familly.Id;
+            }
+            if(stuff!=null)
+            {
+                this.Stuff=stuff;
+                this.StuffId=stuff.Id;
+            }
+            this.CreateDate=DateTime.Now;
+            this.EditDate=DateTime.Now;
+            this.CreatedInCompany=createdInCompany;
+        }
         public int Id { get; set; }
         public string FullName { get; set; }
         public string NameWithoutFamilly { get; set; }
@@ -9,7 +33,7 @@ namespace Domain
         public int ArticleTypeId { get; set; }
         public ArticleType ArticleType { get; set; }
         public int? FamillyId { get; set; }
-        public Familly Familly { get; set; } 
+        public Familly Familly { get; set; }
         public DateTime EditDate { get; set; }
         public DateTime CreateDate { get; set; }
         public int? StuffId { get; set; }
@@ -23,23 +47,23 @@ namespace Domain
         public int High { get; set; } = 0;
         public float Area { get; set; } = 0;
         public float Capacity { get; set; } = 0;
-        public bool CompletedInCompany { get; set; } = true;
         public bool CreatedInCompany { get; set; } = true;
-        public bool HasInfluenceOnOrder { get; set; } = true;
-        public ICollection<ArticleProductionDepartment> ProductionDepartments { get; set; }= new List<ArticleProductionDepartment>();
-        public ICollection<CompanyArticle> Companies { get; set; }= new List<CompanyArticle>();
+        public bool HasChild { get; set; } = false;
+        public bool HasChildSameArticleType { get; set; } = false;
+        public ICollection<ArticleProductionDepartment> ProductionDepartments { get; set; } = new List<ArticleProductionDepartment>();
+        public ICollection<CompanyArticle> Companies { get; set; } = new List<CompanyArticle>();
         public ICollection<ArticleFabricRealization> Realizations { get; set; } = new List<ArticleFabricRealization>();
-        public virtual List <ArticleArticle> ChildRelations { get; set; }
-        public virtual List <ArticleArticle> ParentRelations { get; set; }
+        public virtual List<ArticleArticle> ChildRelations { get; set; }
+        public virtual List<ArticleArticle> ParentRelations { get; set; }
 
         public void CalculateCapacity()
         {
-            if(this.Width!=0 & this.Length!=0)
-                this.Area=this.Width*this.Length;
-            if(this.Area!=0 && this.High!=0)
-                this.Capacity=this.Area*this.High;
+            if (this.Width != 0 & this.Length != 0)
+                this.Area = this.Width * this.Length;
+            if (this.Area != 0 && this.High != 0)
+                this.Capacity = this.Area * this.High;
             return;
-            
+
         }
     }
 }
--- Domain/ArticleArticle.cs
+++ Domain/ArticleArticle.cs
@@ -12,13 +12,11 @@ namespace Domain
             ChildId=child.Id;
             ChildArticle=child;
             Quanity=quanity;
-            PositionOnList=position;
         }
         public int ParentId { get; set; }
         public virtual Article ParentArticle { get; set; }
         public int ChildId { get; set; }
         public virtual Article ChildArticle { get; set; }
-        public int PositionOnList { get; set; }
         public int Quanity { get; set; }
         public int AddCol { get; set; }
 
