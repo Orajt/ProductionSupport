71719a05b24251cbfaf4e8f23473a624b69b51c4
Orajt <marcin.jasiukiewicz1993@gmail.com>
2022-03-22 16:43:37 +0100

    Chaanges in stuff entity, delete stuffcode property

--- Domain/Stuff.cs
+++ Domain/Stuff.cs
@@ -4,7 +4,6 @@ namespace Domain
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int StuffCode { get; set; }
         public int ArticleTypeId { get; set; }
         public ArticleType ArticleType { get; set; }
         public ICollection<Article> Articles { get; set; }=new List<Article>();
